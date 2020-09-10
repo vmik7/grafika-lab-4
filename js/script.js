673a9ce8c8b203145ed18af110634644678df081
Ivan Mikerin <vmik7@yandex.ru>
2020-09-10 13:58:37 +0300

    Logo finished

--- js/script.js
+++ js/script.js
@@ -2,13 +2,63 @@
 
 let canvas = new fabric.Canvas('canvas');
 
-let rect = new fabric.Rect({
-    left: 100,
-    top: 100,
-    fill: '#000',
-    width: 100,
-    height: 60,
-});
+// Цвет заливки
+let logoColor = '#F05133';
 
+// Фоновый круг
+let backgroundCircle = new fabric.Circle({
+    radius: 250, 
+    fill: logoColor, 
+    left: 250,
+    top: 250
+});
+canvas.add(backgroundCircle);
+
+// Основной прямоугольник со скругленными углами
+let rect = new fabric.Rect({
+    left: 250,
+    top: 250,
+    fill: '#fff',
+    width: 220,
+    height: 220,
+    angle: 45,
+    rx: 20,
+    ry: 20
+});
 canvas.add(rect);
 
+// Координаты точек
+let circlesPositions = [
+    { x: 250, y: 185 },
+    { x: 315, y: 250 },
+    { x: 250, y: 315 }
+];
+circlesPositions.forEach((item) => {
+    let smallCircle = new fabric.Circle({
+        radius: 25,
+        fill: logoColor,
+        left: item.x,
+        top: item.y
+    });
+    canvas.add(smallCircle);
+});
+
+// Вертикальная линия
+let verticalLine = new fabric.Line(
+    [250, 185, 250, 315],
+    {
+        strokeWidth: 20,
+        fill: logoColor
+    }
+);
+canvas.add(verticalLine);
+
+// Линия под 45
+let angleLine = new fabric.Line(
+    [204, 139, 315, 250],
+    {
+        strokeWidth: 20,
+        fill: logoColor
+    }
+);
+canvas.add(angleLine);
