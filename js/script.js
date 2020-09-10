9dc67a63696f2eeac2e45705af884ae8bb3cd4a5
Ivan Mikerin <vmik7@yandex.ru>
2020-09-10 13:12:21 +0300

    Included fabric js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let canvas = new fabric.Canvas('canvas');
+
+let rect = new fabric.Rect({
+    left: 100,
+    top: 100,
+    fill: '#000',
+    width: 100,
+    height: 60,
+});
+
+canvas.add(rect);
+
