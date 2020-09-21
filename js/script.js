713cd1ec99d866f150f64ca323c83cd0c88841d5
Ivan Mikerin <vmik7@yandex.ru>
2020-09-21 23:41:35 +0300

    Added animation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 "use strict"
 
-let canvas = new fabric.Canvas('canvas');
+let canvas = new fabric.Canvas('canvas', {
+    width: 500,
+    height: 500
+});
 
 // Цвет заливки
 let logoColor = '#F05133';
 
+// Второй цвет для анимации
+let secondColor = '#24292E';
+
+// Функция анимации
+let easingInName = 'easeInOutCubic';
+let easingOutName = 'easeInOutCubic';
+
 // Фоновый круг
 let backgroundCircle = new fabric.Circle({
-    radius: 250, 
+    selectable: false,
+    radius: 220, 
     fill: logoColor, 
+    originX: 'center',
+    originY: 'center',
     left: 250,
     top: 250
 });
@@ -16,6 +29,9 @@ canvas.add(backgroundCircle);
 
 // Основной прямоугольник со скругленными углами
 let rect = new fabric.Rect({
+    selectable: false,
+    originX: 'center',
+    originY: 'center',
     left: 250,
     top: 250,
     fill: '#fff',
@@ -25,7 +41,7 @@ let rect = new fabric.Rect({
     rx: 20,
     ry: 20
 });
-canvas.add(rect);
+// canvas.add(rect);
 
 // Координаты точек
 let circlesPositions = [
@@ -33,32 +49,156 @@ let circlesPositions = [
     { x: 315, y: 250 },
     { x: 250, y: 315 }
 ];
-circlesPositions.forEach((item) => {
-    let smallCircle = new fabric.Circle({
+let smallCircles = [];
+circlesPositions.forEach((item, i) => {
+    smallCircles[i] = new fabric.Circle({
+        selectable: false,
+        originX: 'center',
+        originY: 'center',
         radius: 25,
         fill: logoColor,
         left: item.x,
         top: item.y
     });
-    canvas.add(smallCircle);
+    // canvas.add(smallCircles[i]);
 });
 
 // Вертикальная линия
 let verticalLine = new fabric.Line(
     [250, 185, 250, 315],
     {
+        selectable: false,
+        originX: 'center',
+        originY: 'center',
         strokeWidth: 20,
-        fill: logoColor
+        stroke: logoColor
     }
 );
-canvas.add(verticalLine);
+// canvas.add(verticalLine);
 
 // Линия под 45
 let angleLine = new fabric.Line(
     [204, 139, 315, 250],
     {
+        selectable: false,
+        originX: 'center',
+        originY: 'center',
         strokeWidth: 20,
-        fill: logoColor
+        stroke: logoColor
     }
 );
-canvas.add(angleLine);
+// canvas.add(angleLine);
+
+// Группируем лого
+let gitLogo = new fabric.Group([
+    rect, smallCircles[0], smallCircles[1], smallCircles[2], verticalLine, angleLine
+], {
+    selectable: false,
+    originX: 'center',
+    originY: 'center',
+    centeredRotation: true
+});
+canvas.add(gitLogo);
+
+// // Анимация появления
+// let start = () => {
+    
+// }
+
+// Анимация основная
+let animationIn = () => {
+    backgroundCircle.animate('scaleX', 2, {
+        duration: 1000,
+        onChange: canvas.renderAll.bind(canvas),
+        easing: fabric.util.ease[easingInName]
+    });
+    backgroundCircle.animate('scaleY', 2, {
+        duration: 1000,
+        onChange: canvas.renderAll.bind(canvas),
+        easing: fabric.util.ease[easingInName]
+    });
+    [ backgroundCircle, gitLogo.item(1), gitLogo.item(2), gitLogo.item(3) ].forEach((item) => {
+        item.animate('fill', secondColor, {
+            duration: 1000,
+            onChange: canvas.renderAll.bind(canvas),
+            easing: fabric.util.ease[easingInName]
+        });
+    });
+    [ gitLogo.item(4), gitLogo.item(5) ].forEach((item) => {
+        item.animate('stroke', secondColor, {
+            duration: 1000,
+            onChange: canvas.renderAll.bind(canvas),
+            easing: fabric.util.ease[easingInName]
+        });
+    });
+    gitLogo.animate('angle', 180, {
+        duration: 1000,
+        onChange: canvas.renderAll.bind(canvas),
+        easing: fabric.util.ease[easingInName],
+    });
+    gitLogo.animate('scaleX', 1.3, {
+        duration: 1000,
+        onChange: canvas.renderAll.bind(canvas),
+        easing: fabric.util.ease[easingInName],
+    });
+    gitLogo.animate('scaleY', 1.3, {
+        duration: 1000,
+        onChange: canvas.renderAll.bind(canvas),
+        easing: fabric.util.ease[easingInName],
+    });
+    gitLogo.animate('angle', 360, {
+        duration: 1000,
+        onChange: canvas.renderAll.bind(canvas),
+        easing: fabric.util.ease[easingInName],
+        onComplete: () => {
+            setTimeout(animationOut, 500);
+        }
+    });
+};
+
+let animationOut = () => {
+    backgroundCircle.animate('scaleX', 1, {
+        duration: 1000,
+        onChange: canvas.renderAll.bind(canvas),
+        easing: fabric.util.ease[easingOutName]
+    });
+    backgroundCircle.animate('scaleY', 1, {
+        duration: 1000,
+        onChange: canvas.renderAll.bind(canvas),
+        easing: fabric.util.ease[easingOutName]
+    });
+    [ backgroundCircle, gitLogo.item(1), gitLogo.item(2), gitLogo.item(3) ].forEach((item) => {
+        item.animate('fill', logoColor, {
+            duration: 1000,
+            onChange: canvas.renderAll.bind(canvas),
+            easing: fabric.util.ease[easingOutName]
+        });
+    });
+    [ gitLogo.item(4), gitLogo.item(5) ].forEach((item) => {
+        item.animate('stroke', logoColor, {
+            duration: 1000,
+            onChange: canvas.renderAll.bind(canvas),
+            easing: fabric.util.ease[easingOutName]
+        });
+    });
+    gitLogo.animate('scaleX', 1, {
+        duration: 1000,
+        onChange: canvas.renderAll.bind(canvas),
+        easing: fabric.util.ease[easingOutName],
+    });
+    gitLogo.animate('scaleY', 1, {
+        duration: 1000,
+        onChange: canvas.renderAll.bind(canvas),
+        easing: fabric.util.ease[easingOutName],
+    });
+    gitLogo.animate('angle', 0, {
+        duration: 1000,
+        onChange: canvas.renderAll.bind(canvas),
+        easing: fabric.util.ease[easingOutName],
+        onComplete: () => {
+            setTimeout(animationIn, 1000);
+        }
+    });
+};
+
+animationIn();
