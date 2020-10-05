3e9aba61c3313f6f50061dd974c79e372fbfdecd
Ivan Mikerin <vmik7@yandex.ru>
2020-10-05 09:25:41 +0300

    Grid completed

--- js/script.js
+++ js/script.js
@@ -1,75 +1,82 @@
-"use strict"
+"use strict";
 
 // Ширина и высота экрана
+// const vw = Math.min(Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0), document.body.offsetWidth);
+// const vh = Math.min(Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0), document.body.offsetHeight);
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-// Масштаб логотипа
-let sc = Math.min(vh * 0.8 / 500, vw * 0.8 / 500);
+// Фон
+let bgColor = "#fff";
 
+// Цвет сетки
+let gridColor = "#ededed";
 
-let canvas = document.querySelector('.canvas');
-let ctx = canvas.getContext('2d');
- 
+// Цвет осей
+let axesColor = "#232323";
+
+// Цвет графика
+let graphColor = "#f54c4c";
+
+// Масштаб сетки
+let dx = 30;
+
+// Начало координат
+let x0 = vw / 2;
+let y0 = vh / 2;
+
+// Инициализация канваса
+let canvas = document.querySelector(".canvas");
+let ctx = canvas.getContext("2d");
+
+// Растягиваем на весь экран
 canvas.width = vw;
 canvas.height = vh;
 
-let x0 = vw / 2;
-let y0 = vh / 2;
-let dx = 30;
-let dy = 30;
-
-ctx.fillStyle = '#fff';
+// Заливка фоновым цветом
+ctx.fillStyle = bgColor;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-let gridX = () => {
-  let line = (x) => {
-		ctx.strokeStyle = '#999';
-    ctx.setLineDash([ 1, 3 ]);
-    ctx.moveTo(x,0);
-    ctx.lineTo(x,canvas.height);
-    ctx.stroke();
-  }
-  for (let i = x0; i < canvas.width; i += dx) {
-    line(i);
-  }
-  for (let i = x0; i >= 0; i -= dx) {
-    line(i);
-  }
-}
-gridX();
+// Рисует вертикальную линию
+let lineVertical = (x, color = '#000', dash = []) => {
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  // ctx.setLineDash(dash);
+  ctx.moveTo(x, 0);
+  ctx.lineTo(x, canvas.height);
+  ctx.stroke();
+  // ctx.setLineDash([]);
+};
 
-let gridY = () => {
-  let line = (y) => {
-		ctx.strokeStyle = '#999';
-    ctx.setLineDash([ 1, 3 ]);
-    ctx.moveTo(0, y);
-    ctx.lineTo(canvas.width, y);
-    ctx.stroke();
-  }
-  for (let i = y0; i < canvas.height; i += dy) {
-    line(i);
-  }
-  for (let i = y0; i >= 0; i -= dy) {
-    line(i);
-  }
-}
-gridY();
+// Рисует горизонтальную линию
+let lineHorizintal = (y, color = '#000', dash = []) => {
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  // ctx.setLineDash(dash);
+  ctx.moveTo(0, y);
+  ctx.lineTo(canvas.width, y);
+  ctx.stroke();
+  // ctx.setLineDash([]);
+};
 
-let axisX = () => {
-		ctx.strokeStyle = '#000';
-    ctx.setLineDash([]);
-    ctx.moveTo(0, y0);
-    ctx.lineTo(canvas.width, y0);
-    ctx.stroke();
-}
-/* axisX(); */
+// Отрисовка сетки
+let grid = () => {
+  for (let i = x0; i < canvas.width; i += dx)
+    lineVertical(i, gridColor, [5, 10]);
+  for (let i = x0; i >= 0; i -= dx)
+    lineVertical(i, gridColor, [5, 10]);
+  for (let i = y0; i < canvas.height; i += dx) 
+    lineHorizintal(i, gridColor, [5, 10]);
+  for (let i = y0; i >= 0; i -= dx)
+    lineHorizintal(i, gridColor, [5, 10]);
+    
+};
+grid();
 
-let axisY = () => {
-		ctx.strokeStyle = '#000';
-    ctx.setLineDash([]);
-    ctx.moveTo(x0,0);
-    ctx.lineTo(x0,canvas.height);
-    ctx.stroke();
-}
-/* axisY(); */
+// Отрисовка осей
+let axis = () => {
+  lineVertical(x0, axesColor);
+  lineHorizintal(y0, axesColor);
+};
+axis();
+
